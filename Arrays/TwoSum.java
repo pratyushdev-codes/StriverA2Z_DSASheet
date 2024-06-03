25c7cfd4ba497d754878aa4940ac086bd1dcf9a6
Pratyush Birole <birole.pratyush@gmail.com>
2024-06-03 16:27:23 +0530

    Add files via upload

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -4,16 +4,14 @@ import java.util.Map;
 public class TwoSum {
 
     public static void main(String[] args) {
-        
-    }public static int [] twoSum(int arr [], int target){
+          
+    }public static int [] twoSum(int [] sum , int target){
         Map<Integer, Integer> hm = new HashMap<>();
-        for(int i =0; i<arr.length;i++){
-            if(hm.containsKey(target=arr[i])){
-              
-                return new int[] {hm.get(target -arr[i]), i};
-            }
-            hm.put(arr[i], i);
-        }
-        return new int[]{};
+              for(int i = 0 ; i< sum.length; i++){
+                if(hm.containsKey(target=sum[i])){
+                 hm.put(sum[i], i);
+                }
+              }
+        return new int []{};
     }
 }
---
+++ Arrays/UnionofTwoSortedArrays.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+/**
+ * UnionofTwoSortedArrays
+ */
+public class UnionofTwoSortedArrays {
+
+    public static void main(String[] args) {
+        
+    } public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        int p1=arr1[0];
+        int p2 = arr2[0];
+
+        for(int p1 = arr)
+
+
+
+}
+}
