c5661248d132504e98b6bbcef4bce39d75579269
Pratyush Birole <birole.pratyush@gmail.com>
2024-06-09 23:56:41 +0530

    Add files via upload

--- Binary Search/FindMin.java
+++ Binary Search/FindMin.java
@@ -1,5 +1,19 @@
 public class FindMin {
     public static void main(String[] args) {
         
+    }public int findMin(int[] nums) {
+        int start = 0;
+        int end = nums.length-1;
+
+        while(start<=end){
+            int mid = start+ end/2;
+            if(nums[mid]<nums[end]){
+                end= mid;
+            }else{
+                start= mid+1;
+
+            }
+        }
+return nums[start];
     }
 }
