ce55f55b33d42715e9f6292f6fee4c628da45e1a
Pratyush Birole <birole.pratyush@gmail.com>
2024-06-14 15:31:01 +0530

    Add files via upload

---
+++ Recurssion/SumofNNaturalNumbers.java
@@ -0,0 +1,16 @@
+/**
+ * SumofNNaturalNumbers
+ */
+public class SumofNNaturalNumbers {
+
+    public static void main(String[] args) {
+        
+    }public int Sum(int N){
+        if(N==1){
+            return 1;
+        }
+
+        int temp = Sum(N-1);
+        return temp +N;
+    }
+}
