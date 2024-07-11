6bfcc36b8c8f0e97f880d4a4cadb97168c15606e
Pratyush Birole <birole.pratyush@gmail.com>
2024-07-11 09:24:58 +0530

    Add files via upload

---
+++ Priority Queue | Heaps/TaskScheduler.java
@@ -0,0 +1,24 @@
+public class TaskScheduler {
+    public static void main(String[] args) {
+        
+    }class Solution {
+        public int leastInterval(char[] tasks, int p) {
+            //building frequency map:-
+
+            int [] freq = new int[26];
+            for(char ch : tasks){
+                freq[ch-'A']++;
+            }
+
+
+
+
+        }
+    }
+
+
+
+
+
+    
+}
