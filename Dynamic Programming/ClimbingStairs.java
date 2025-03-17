3270196cd447311b16e09cd68a39ef59d5de0f8b
Pratyush Birole  ▲ <birole.pratyush@gmail.com>
2025-03-17 14:33:22 +0530

    Add files via upload

--- Dynamic Programming/ClimbingStairs.java
+++ Dynamic Programming/ClimbingStairs.java
@@ -18,3 +18,21 @@ public class ClimbingStairs {
         return dp[n];
     }
 }
+
+
+// Bottom Up appraoch := 
+
+public class int countMaxWays(int n){
+    if(n==0){
+        return cost[0];
+    }
+    if(n==1){
+        return cost[1];
+    }
+
+    for(int i = 2; i<= n ; i++){
+        dp[i] = cost [i]+ Math.max(dp[i-1], dp[i-2]);
+    }
+
+    return Math.min(dp[n-1], dp[n-2]);
+}
