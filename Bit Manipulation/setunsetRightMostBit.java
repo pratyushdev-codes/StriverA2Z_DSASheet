fe48d3ea6daa0ab8edd62fe2f5c333baef99ca3c
Pratyush Birole  ▲ <birole.pratyush@gmail.com>
2025-06-01 19:21:21 +0530

    Add files via upload

---
+++ Bit Manipulation/setunsetRightMostBit.java
@@ -0,0 +1,7 @@
+public class setunsetRightMostBit {
+    public static void main(String[] args) {
+        int number = 10;
+    }public int setRightMostBit(int n){
+        return n | (n+1);
+    }pu
+}
