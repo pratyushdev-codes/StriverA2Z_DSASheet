deca7be0b9933a14037d5913eae30b9c483035a5
Pratyush ▲ <birole.pratyush@gmail.com>
2025-09-26 11:25:25 +0530

    Add files via upload

--- Linked List/ReverseLLIterative.java
+++ Linked List/ReverseLLIterative.java
@@ -21,29 +21,31 @@
 //    }
 
 
-//revser a LL:-
+//reverse a LL:-
+class Node{
+    int data;
+    Node next;
 
+    Node(int data){
+        this.data = data;
+        this.next= null;
+    }
+}
 
 public class ReverseLLIterative {
-
-    public static void main(String[] args) {
-        
-    }    public ListNode reverseList(ListNode head) {
-        if(head==null || head.next ==null){
-            return head;
-        }
-
-        ListNode prev= null;
-        ListNode curr = head;
-        while(curr.next!=null){
-            ListNode nextNode= curr.next;
-            curr.next = prev;
-            prev= curr;
-            curr= nextNode;
+    public static Node reverse(Node head){
+        Node prev = null;
+        Node current = head;
+        Node nextNode ;
+        while(current!=null){
+            nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current=nextNode;
         }
         return prev;
 
-
-
+    
     }
+    
 }
