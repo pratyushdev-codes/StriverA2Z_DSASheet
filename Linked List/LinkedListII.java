065b658c1f269c4a9b5b817d0b517afdcdf27b3d
Pratyush ▲ <birole.pratyush@gmail.com>
2025-09-27 20:31:55 +0530

    Add files via upload

---
+++ Linked List/LinkedListII.java
@@ -0,0 +1,25 @@
+public class LinkedListII {
+    public ListNode detectCycle(ListNode head){
+         ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        if (fast == null || fast.next == null) return null;
+
+        fast = head;
+
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+} 
+    }
+}
