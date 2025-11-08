ba654f7172bfa635b7926d8f1ad7c393f36b9cbc
Pratyush ▲ <birole.pratyush@gmail.com>
2025-11-08 15:45:41 +0530

    Add files via upload

--- Dynamic Programming/LargestDivisibleSubset.java
+++ Dynamic Programming/LargestDivisibleSubset.java
@@ -1,3 +1,23 @@
 public class LargestDivisibleSubset {
-    
+    public static void main(String[] args) {
+        
+    }public List<Integer> LargestDivisibleSubset(int arr []){
+        Arrays.sort(arr);
+        int dp [] = new int [arr.length];
+        List<Integer> result = new ArrayList<>();
+
+    }private void helper(int[] nums, int index, List<Integer> curr, int prev) {
+        if(curr.size() > res.size()) {
+            res = new ArrayList<>(curr);
+        }
+
+        for(int i=index; i<nums.length; i++) {
+            if(curr.size() > mem[i] && nums[i]%prev == 0) {
+                mem[i] = curr.size();
+                curr.add(nums[i]);
+                helper(nums, i+1, curr, nums[i]);
+                curr.remove(curr.size()-1);
+            }
+        }
+    }
 }
