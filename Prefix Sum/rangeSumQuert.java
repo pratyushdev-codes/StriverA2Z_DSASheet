073937c96d4f02bd414df8f9d6e3d1c2d299da1f
Pratyush ▲ <birole.pratyush@gmail.com>
2025-12-02 22:59:17 +0530

    Add files via upload

--- Prefix Sum/rangeSumQuert.java
+++ Prefix Sum/rangeSumQuert.java
@@ -11,12 +11,7 @@ public class rangeSumQuert {
         for(int i = 0 ; i< n ; i++){
             pf[i] = pf[i-1]+ nums[i];
         }
-    }public int sumRange(int left, int right){
-        if(left>0){
-            return pf[right] - pf[left-1];
-        }else{
-            return pf[right];
-        }
+    }
 
     }
 }
